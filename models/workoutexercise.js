f03c9c0ffb3f2324574d60ca22f5c98a8be1408d
João Gabriel <jgcustodio2005@gmail.com>
2025-04-07 16:54:57 -0300

    fix(sessionController): session controller backup amem

--- models/workoutexercise.js
+++ models/workoutexercise.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
   class WorkoutExercise extends Model {
     static associate(models) {
       this.belongsTo(models.WorkoutSession, {
-        foreignKey: 'workout_id',
+        foreignKey: 'workout_session_id',
         as: 'workout_session'
       })
 
@@ -14,7 +14,7 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'workout_exercise_id',
         as: 'workout_sets'
       })
-      
+
 
       this.belongsTo(models.Exercise, {
         foreignKey: 'exercise_id',
@@ -23,34 +23,34 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   WorkoutExercise.init({
-    workout_id: {
-      type:  DataTypes.INTEGER,
+    workout_session_id: {
+      type: DataTypes.INTEGER,
       allowNull: false
     },
     exercise_id: {
-      type:  DataTypes.INTEGER,
+      type: DataTypes.INTEGER,
       allowNull: false
     },
     sets: {
-      type:  DataTypes.INTEGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       validate: {
         min: 1
       }
     },
     reps: {
-      type:  DataTypes.INTEGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       validate: {
         min: 1
       }
     },
     weight: {
-      type:  DataTypes.FLOAT,
+      type: DataTypes.FLOAT,
       allowNull: true
     },
     notes: {
-      type:  DataTypes.TEXT,
+      type: DataTypes.TEXT,
       allowNull: true
     }
   }, {
