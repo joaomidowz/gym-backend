ad0e4fa4de5198caee4fd5d7f97a29f31756e531
João Gabriel <jgcustodio2005@gmail.com>
2025-04-09 15:29:40 -0300

    fix(follow): fixing the follow controller and routos to stop followin and verify if is followin

--- middlewares/isFollowOwner.js
+++ middlewares/isFollowOwner.js
@@ -1,19 +1,22 @@
-const { Follow } = require('../models')
+const { Follow } = require('../models');
 
 const isFollowOwner = async (req, res, next) => {
-    const followerId = req.user.id
-    const followingId = paresInt(req.params.userId)
+    const followerId = req.user.id;
+    const followingId = parseInt(req.params.userId); 
 
     try {
-        const follow = await Follow.findOnde({
+        const follow = await Follow.findOne({ 
             where: { follower_id: followerId, following_id: followingId }
-        })
+        });
 
-        if (!follow) return res.status(403).json({ message: 'You do not have permission to alter' })
-        next()
+        if (!follow) {
+            return res.status(403).json({ message: 'You do not have permission to alter' });
+        }
+
+        next();
     } catch (error) {
         return res.status(500).json({ error: error.message });
     }
-}
+};
 
 module.exports = isFollowOwner;
