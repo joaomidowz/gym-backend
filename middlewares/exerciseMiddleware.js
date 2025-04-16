4d1a2793eaec45c3a0bd98d55e88efaa3b133664
João Gabriel <jgcustodio2005@gmail.com>
2025-04-16 15:19:04 -0300

    fix(middleware): allow admin to edit/delete any exercise including global ones

--- middlewares/exerciseMiddleware.js
+++ middlewares/exerciseMiddleware.js
@@ -4,13 +4,15 @@ const isExerciseOwnerOrAdmin = async (req, res, next) => {
     try {
         const exercise = await Exercise.findByPk(req.params.id)
 
-        if (!exercise) return res.status(404).json({ message: 'Exercise not found.'})
+        if (!exercise) return res.status(404).json({ message: 'Exercise not found.' })
 
-        if (req.user.is_admin || exercise.user_id === req.user.id) return next()
+        if (req.user?.is_admin || exercise?.user_id === req.user?.id) {
+            return next();
+        }
 
-        return res.status(403).json({ error: 'Access denied. You are not the owner of this exercise.'})
+        return res.status(403).json({ error: 'Access denied. You are not the owner of this exercise.' })
     } catch (error) {
-        return res.status(500).json({ error: 'Error to verify the propriety of this exercise'})
+        return res.status(500).json({ error: 'Error to verify the propriety of this exercise' })
     }
 }
 
@@ -18,27 +20,29 @@ const isExerciseGlobal = async (req, res, next) => {
     try {
         const exercise = await Exercise.findByPk(req.params.id)
 
-        if (!exercise) return res.status(400).json({ message: 'Exercise not found.'})
+        if (!exercise) return res.status(400).json({ message: 'Exercise not found.' })
 
-        if (exercise.is_global) return res.status(403).json({ error: 'Global exercise cannot be edited or deleted'})
+        if(exercise.is_global && !req.user.is_admin) {
+            return res.status(403).json({ error: 'Global exercise cannot be edited or deleted' })
+        }
 
         return next()
     } catch (error) {
-        return res.status(500).json({ error: 'Error to verify the propriety of this exercise'})
+        return res.status(500).json({ error: 'Error to verify the propriety of this exercise' })
     }
 }
 
 const isExerciseInUse = async (req, res, next) => {
     try {
         const count = await WorkoutExercise.count({
-            where: {exercise_id: req.params.id}
+            where: { exercise_id: req.params.id }
         })
 
-        if (count > 0) return res.status(400).json({ message: 'Exercise in use. Cant be edited.'})
+        if (count > 0) return res.status(400).json({ message: 'Exercise in use. Cant be edited.' })
 
         return next()
     } catch (error) {
-        return res.status(500).json({ error: 'Error to verify the exercise is in use'})
+        return res.status(500).json({ error: 'Error to verify the exercise is in use' })
     }
 }
 
